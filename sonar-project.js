b1a0251e8f26f1659185e3905cffa98e133fbdd2
ommaxyl <105895941+ommaxyl@users.noreply.github.com>
2022-07-20 03:03:46 -0600

    updated server url and password

--- sonar-project.js
+++ sonar-project.js
@@ -1,12 +1,12 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://3.145.6.52:7000/',
+  serverUrl: 'http://3.21.53.186:7000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsAcadalearning',
-	    'sonar.login':'c49bf112bb91004bb4a3ca9f6c62713d30723a01'
+	    'sonar.login':'8211f3013a5277a7c4d3be14d1cdb272b4f4bfcd'	       
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
